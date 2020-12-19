d2db4fe44c55ec39a8a35d8b62ac2a5cae493d75
tracyhenry <thierryhenrytracy@163.com>
2020-12-18 17:11:14 -0800

    SchemaGraph: replaced hardcoded arrows with antd icons.

--- client/src/js/SchemaGraph.js
+++ client/src/js/SchemaGraph.js
@@ -4,6 +4,7 @@ import {resizeSvgs} from "./ResizeStuff";
 import NodePopover from "./low-level-components/NodePopover";
 import EdgePopover from "./low-level-components/EdgePopover";
 import {Button, Space} from "antd";
+import {createFromIconfontCN} from "@ant-design/icons/lib/index";
 
 class SchemaGraph extends Component {
     constructor(props) {
@@ -70,6 +71,11 @@ class SchemaGraph extends Component {
             .on("tick", tickFunction.bind(this))
             .on("end", endFunction.bind(this))
             .stop();
+
+        // iconfont
+        this.IconFont = createFromIconfontCN({
+            scriptUrl: "//at.alicdn.com/t/font_2257494_inodlmuktwo.js"
+        });
     }
 
     componentDidMount = () => {
@@ -588,80 +594,32 @@ class SchemaGraph extends Component {
 
         const jumpStartZoom = jump => {
             if (jump.type !== "semantic_zoom") return;
-            let circleRadius = this.circleRadius;
-            var nodes = graphMainSvg.selectAll("circle");
-            var currentNode = nodes.filter(d =>
-                d.table_name === this.props.curTable ? true : false
-            );
-            var arrowG = graphMainSvg
-                .select("g")
-                .append("g")
-                .attr("id", "arrowG")
-                .attr(
-                    "transform",
-                    `translate(${+currentNode.attr("cx") +
-                        circleRadius}, ${+currentNode.attr("cy") +
-                        (jump.backspace
-                            ? circleRadius - 10
-                            : -circleRadius + 10)}) scale(0.04, ${
-                        jump.backspace ? -0.04 : 0.04
-                    })`
-                );
-            arrowG
-                .append("path")
-                .attr(
-                    "d",
-                    "M1010.81001246 56.6447675c8.13102537-10.31546098 7.28974996-25.0705531-1.95511535-34.39404215-9.24748529-9.32741999-23.99471553-10.29580432-34.37962739-2.25650648L661.20604861 333.26081902 347.93944847 19.99290792a25.36932426 25.36932426 0 0 0-36.9663555 0 31.35260638 31.35260638 0 0 0-7.82963326 18.48317774 31.33032877 31.33032877 0 0 0 7.82963326 18.16737184l331.75108886 331.12471705a25.36932426 25.36932426 0 0 0 36.96635453 0L1010.81001246 56.6447675z m0 0"
+            let curRect = graphMainSvg
+                .selectAll("circle")
+                .filter(d =>
+                    d.table_name === this.props.curTable ? true : false
                 )
-                .attr("fill", "#d81e06");
-            arrowG
-                .append("path")
-                .attr(
-                    "d",
-                    "M1060.30902307 306.94800705c10.20669774-11.8027639 9.56853309-29.48528762-1.46371531-40.51622603-11.03093841-11.03093841-28.71215214-11.67041306-40.51622604-1.46240436l-357.12303311 357.12172215-357.12565405-357.12172215a29.12492746 29.12492746 0 0 0-41.9760104 0 32.88839353 32.88839353 0 0 0-8.77050002 20.9886602 31.32639783 31.32639783 0 0 0 8.77050002 20.98997019l378.11300426 378.11169427a29.13803126 29.13803126 0 0 0 41.97863136 0l378.11300329-378.11169427z m0 0"
-                )
-                .attr("fill", "#d81e06");
-            arrowG
-                .append("path")
-                .attr(
-                    "d",
-                    "M1110.50385639 567.09365422c11.488268-13.28613491 10.76885866-33.18716468-1.6484823-45.60450564-12.41602999-12.41602999-32.31574882-13.13412935-45.60188468-1.64455039l-401.95178135 401.95178135-401.95702422-401.95178135a32.7848722 32.7848722 0 0 0-47.24512412 0 37.02794352 37.02794352 0 0 0-9.87254614 23.62387253 35.25759515 35.25759515 0 0 0 9.87254614 23.62387253l425.57827484 425.57696484a32.79404505 32.79404505 0 0 0 47.24774603 0l425.5782758-425.57565387z m0 0"
-                )
-                .attr("fill", "#d81e06");
-
-            // a cover-up rectangle
-            var coverRect = graphMainSvg
-                .select("g")
-                .append("rect")
-                .attr("id", "coverrect")
-                .attr("x", +currentNode.attr("cx") + circleRadius + 5)
-                .attr("y", +currentNode.attr("cy") - circleRadius)
-                .attr("width", 50)
-                .attr("height", circleRadius * 2)
-                .attr("fill", "white");
-            var duration = 700;
+                .node()
+                .getBoundingClientRect();
+            console.log(curRect);
+            let selector = jump.backspace ? ".arrow-up" : ".arrow-down";
+            let arrowIcon = d3
+                .selectAll(selector)
+                .style("visibility", "visible")
+                .style("left", curRect.x + curRect.width + "px")
+                .style("top", curRect.y + "px")
+                .style("opacity", 0);
+            var duration = 300;
             function repeat() {
-                if (jump.backspace)
-                    coverRect
-                        .transition()
-                        .duration(duration)
-                        .attr("height", 0)
-                        .on("end", function() {
-                            coverRect.attr("height", circleRadius * 2);
-                            repeat();
-                        });
-                else
-                    coverRect
-                        .transition()
-                        .duration(duration)
-                        .attr("y", +currentNode.attr("cy") + circleRadius)
-                        .on("end", function() {
-                            coverRect.attr(
-                                "y",
-                                +currentNode.attr("cy") - circleRadius
-                            );
-                            repeat();
-                        });
+                arrowIcon
+                    .transition()
+                    .duration(duration)
+                    .style("opacity", 1)
+                    .transition()
+                    .style("opacity", 0)
+                    .on("end", function() {
+                        repeat();
+                    });
             }
             d3.select("body").style("pointer-events", "none");
             repeat();
@@ -669,11 +627,10 @@ class SchemaGraph extends Component {
 
         const jumpEndZoom = jump => {
             if (jump.type !== "semantic_zoom") return;
-            var coverRect = d3.select("#coverrect");
-            if (coverRect.empty()) return;
-            coverRect.interrupt();
-            coverRect.remove();
-            d3.select("#arrowG").remove();
+            let selector = jump.backspace ? ".arrow-up" : ".arrow-down";
+            d3.selectAll(selector)
+                .interrupt()
+                .style("visibility", "hidden");
             d3.select("body").style("pointer-events", "auto");
         };
 
@@ -762,7 +719,16 @@ class SchemaGraph extends Component {
                     </Space>
                 </div>
                 <>{this.getPopovers()}</>
-                <div className="explain">Schema Graph View</div>
+                <this.IconFont
+                    type="icon-triple-arrow-up"
+                    className="graph-arrow arrow-up"
+                    style={{visibility: "hidden"}}
+                />
+                <this.IconFont
+                    type="icon-triple-arrow-down"
+                    className="graph-arrow arrow-down"
+                    style={{visibility: "hidden"}}
+                />
             </div>
         );
     }
