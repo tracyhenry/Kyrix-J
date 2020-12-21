d73a65e9ec40e825371b25e54f04c82cf07f32c2
tracyhenry <thierryhenrytracy@163.com>
2020-12-20 18:32:22 -0800

    setup proxy between react dev server and express-pg middleware.

--- client/auto-complete/index.js
+++ client/auto-complete/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3001;
 const psql = require("pg");
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
     let client = new psql.Client({
         host: "localhost",
         user: "wenbo",
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
     client.connect();
     client.query("SELECT count(*) from plays;", (err, result) => {
         res.status(200).send(
-            "Total rows in the plays table: " + JSON.stringify(result)
+            "Total rows in the plays table: " + result.rows[0].count
         );
     });
 });
