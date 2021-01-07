8e60195b8073df63649be8df59a1492d17b4f917
tracyhenry <thierryhenrytracy@163.com>
2021-01-07 15:51:47 -0800

    Authoring: import input json

---
+++ authoring/index.js
@@ -0,0 +1,4 @@
+const appName = "mitdwh";
+const vis = require(`./apps/${appName}/input/vis.json`);
+const pk = require(`./apps/${appName}/input/pk.json`);
+const graph = require(`./apps/${appName}/input/graph.json`);
